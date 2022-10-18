d5e7299d6efa9c6616e053a3c1b76e215e8c68d2
murasaqi <uma9266@gmail.com>
2022-10-19 00:03:14 +0900

    Add base project

---
+++ AnimationCurveManeuver/Assets/AnimationCurveManeuver/Editor/AnimationTrackUtility.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+namespace iridescent.AnimationCurveManeuver
+{
+    public static class AnimationTrackUtility
+    {
+       
+    }
+}
